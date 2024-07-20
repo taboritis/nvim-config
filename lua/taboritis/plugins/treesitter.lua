2c013165fbd3e3ccef8a02f6f50e64b7f181f1b3
Piotr Skrobol <taboritis@gmail.com>
2024-07-20 21:27:12 +0200

    Add bufferline and dressing plugins, update lualine styles

--- lua/taboritis/plugins/treesitter.lua
+++ lua/taboritis/plugins/treesitter.lua
@@ -1,14 +1,55 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
+	"nvim-treesitter/nvim-treesitter",
+	event = { "BufReadPre", "BufNewFile" },
+	build = ":TSUpdate",
 
-    configs.setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "query", "javascript", "html", "php" },
-      sync_install = false,
-      highlight = { enable = true },
+	config = function()
+		local configs = require("nvim-treesitter.configs")
+
+		configs.setup({
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "yaml",
+        "html",
+        "css",
+        "prisma",
+        "markdown",
+        "markdown_inline",
+        "svelte",
+        "graphql",
+        "bash",
+        "lua",
+        "vim",
+        "dockerfile",
+        "gitignore",
+        "php",
+        "query",
+        "vim",
+        "vimdoc",
+        "c",
+      },
+			sync_install = false,
+      highlight = {
+        enable = true,
+      },
+      -- enable indentation
       indent = { enable = true },
-    })
-  end
+      -- enable autotagging (w/ nvim-ts-autotag plugin)
+      autotag = {
+        enable = true,
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+		})
+	end,
 }
